af2e53db04b77b1671cf513bdc7de3320575aa4b
CervellinMateo <mateo.cervellin@heig-vd.ch>
2025-09-16 12:32:54 +0000

    added TD20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+
+	unsigned int num_students = 23;
+	// signed int m = -42; // signed est optionelle
+	int account_value = -42; // est aussi corect
+	float temperature = 18.1; //32 bit
+	double pi = 3.141592653589793; // 64 bit
+}
