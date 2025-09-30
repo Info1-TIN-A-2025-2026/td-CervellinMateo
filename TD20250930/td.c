0d7dde2e0683779a12b8f83f90053b68245f2a63
CervellinMateo <mateo.cervellin@heig-vd.ch>
2025-09-30 12:32:51 +0000

    added TD20250930

---
+++ TD20250930/td.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	double m1 = 0;
+	const double G = 6.67e30;
+	printf("Calculateur de force gravitationnel:\n\tMasse1(kg): ");
+	scanf("%lf", &m1);
+	puts(m1);
+}
