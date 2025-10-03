9dc193278156e1b20c6742c8e89a58d4bbc56066
CervellinMateo <mateo.cervellin@heig-vd.ch>
2025-10-03 08:27:43 +0000

    almost finished

--- TD20250930/td.c
+++ TD20250930/td.c
@@ -6,16 +6,23 @@ int main(int argc, const char *argv[])
 	double m2 = 0;
 	double dist = 0;
 	double F = 0;
-	const double G = 6.67e30;
+	const double G = 6.67e-11;
 	printf("Calculateur de force gravitationnel:\n\tMasse1(kg): ");
 	scanf("%lf", &m1);
 	printf("\tMasse2(kg):");
 	scanf("%lf", &m2);
-	printf("\tDistance(m)");
+	printf("\tDistance(m):");
 	scanf("%lf", &dist);
-
-	if ((m1 | m2 | dist) < 0)
+	if ((m1 < 0) | (m2 < 0) | (dist < 0))
 	{
 		puts("ERROR");
+	} else if ((m1 > 1.7e308) | (m2 > 1.7e308) | (dist > 1.7e308))
+	{
+		puts("OVERFLOW");
+	}
+	else
+	{
+		F = (G * m1 * m2) / (dist * dist);
+		printf("La force est de %.2e N.\n", F);
 	}
 }
